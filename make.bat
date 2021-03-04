090cac064e8cf42dd8edafe2157dff34bbc1fefb
chongkx <chongkx@glodon.com>
2021-03-04 15:47:28 +0800

    tianjai

---
+++ make.bat
@@ -0,0 +1,56 @@
+::文件内容   执行ThirdPart_Conan_Linux_aarch64.bat 文件
+::执行方法  make.bat D:\GLD\gcr\ThirdPart C:\Users\chongkx\.conan\data
+::位置：用户/xxx/.conan
+::[参数1]：ThirdPart路径
+::[参数2]：GLDRS路径
+
+
+SET ThirdPart=%1
+SET GLDRS=%2
+
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% FreeImage/3.18.0 freeImage FreeImage\3.18.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% boost/1.65.1 boost boost\1.65.1
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% brotli/1.0.7 brotli brotli\1.0.7
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% cpp4x/1.0 cpp4x cpp4x\1.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% freetype/2.4.9 freetype freetype\2.4.9
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% gperftools/2.6.90 gperftools gperftools\2.6.90
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% gtest/1.8.0 gtest gtest\1.8.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% jsoncpp/1.6.2 jsoncpp jsoncpp\1.6.2
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% log4cplus/2.0.0 log4cplus log4cplus\2.0.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% qca/2.1.0 qca qca\2.1.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% cryptopp/6.1.0 cryptopp cryptopp\6.1.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% easyhook\2.7.6270.0 easyhook\2.7.6270.0 easyhook\2.7.6270.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% libxml/2.7.3 libxml libxml\2.7.3
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% libzip/1.2.0 libzip libzip\1.2.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% zlib/1.2.7 zlib zlib\1.2.7
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% quazip/0.5.1 quazip quazip\0.5.1
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% richtext/20110923 richtext richtext\20110923
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% VSDockControl/1.0 VSDockControl VSDockControl\1.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% QtitanRibbon/3.3.0 QtitanRibbon QtitanRibbon\3.3.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% lzma/9.20 lzma lzma\9.20
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% curl/7.60.0 curl curl\7.60.0
+
+
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% libxl/3.7.2.2 libxl libxl\3.7.2.2
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% sqlite3/3.20.1 sqlite3 sqlite3\3.20.1
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% libzip/1.2.0 libzip libzip\1.2.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% lua/5.3.1 lua lua\5.3.1
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% FFmpeg/2.2.16 ffmpeg FFmpeg\2.2.16
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% flex/2.5.4a flex flex\2.5.4a
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% libiconv/1.15 libiconv libiconv\1.15
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% mockcpp/2.6.0 mockcpp mockcpp\2.6.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% msgpack/1.0.0 msgpack msgpack\1.0.0
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% tbb/4.3update3 tbb tbb\4.3update3
+call ThirdPart_Conan_Linux_aarch64.bat %ThirdPart% %GLDRS% rapidjson/1.1.0 rapidjson rapidjson\1.1.0
+
+
+pause
+
+
+
+
+
+
+
+
+
